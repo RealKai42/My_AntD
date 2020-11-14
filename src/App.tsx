7b85f8ccf7965488b5a81dde493948bd4852e5c8
KaiyiWing <Zhang.Kaiyi42@gmail.com>
2020-11-14 13:44:16 +0800

    完成样式系统，添加样式变量、reboot.scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello world</h1>
+        <h2>Hello world</h2>
+        <h3>Hello world</h3>
+        <hr />
+        <code>const a = 'b'</code>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -20,7 +22,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
