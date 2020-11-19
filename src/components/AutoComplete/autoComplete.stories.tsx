261f4b4050dfdc3915b3f20c7767507838ce72c2
KaiyiWing <Zhang.Kaiyi42@gmail.com>
2020-11-19 19:35:28 +0800

    autoComplete支持异步

--- src/components/AutoComplete/autoComplete.stories.tsx
+++ src/components/AutoComplete/autoComplete.stories.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 // import { action } from '@storybook/addon-actions'
-import { AutoComplete } from './autoComplete'
+import { AutoComplete, DataSourceType } from './autoComplete'
 import { action } from '@storybook/addon-actions'
+
+interface LakerPlayerProps {
+  value: string
+  number: number
+}
+interface GithubUserProps {
+  login: string
+  url: string
+  avatar_url: string
+}
 
 const SimpleComplete = () => {
   const lakers = [
@@ -20,14 +30,59 @@ const SimpleComplete = () => {
     'rando',
   ]
 
+  const lakersWithNumber = [
+    { value: 'bradley', number: 11 },
+    { value: 'pope', number: 1 },
+    { value: 'caruso', number: 4 },
+    { value: 'cook', number: 2 },
+    { value: 'cousins', number: 15 },
+    { value: 'james', number: 23 },
+    { value: 'AD', number: 3 },
+    { value: 'green', number: 14 },
+    { value: 'howard', number: 39 },
+    { value: 'kuzma', number: 0 },
+  ]
+
+  // const handleFetch = (query: string) => {
+  //   return lakers.filter((name) => name.includes(query)).map(name=> ({value:name}))
+  // }
+
+  // const handleFetch = (query: string) => {
+  //   return lakersWithNumber.filter((player) => player.value.includes(query))
+  // }
+
   const handleFetch = (query: string) => {
-    return lakers.filter((name) => name.includes(query))
+    return fetch(`https://api.github.com/search/users?q=${query}`)
+      .then((res) => res.json())
+      .then(({ items }) => {
+        return items
+          .slice(0, 10)
+          .map((item: any) => ({ value: item.login, ...item }))
+      })
   }
 
+  // const renderOption = (item: DataSourceType<LakerPlayerProps>) => {
+  //   return (
+  //     <>
+  //       <h2>Name:{item.value}</h2>
+  //       <p>Number:{item.number}</p>
+  //     </>
+  //   )
+  // }
+
+  const renderOption = (item: DataSourceType<GithubUserProps>) => {
+    return (
+      <>
+        <h2>Name:{item.login}</h2>
+        <p>Number:{item.url}</p>
+      </>
+    )
+  }
   return (
     <AutoComplete
       fetchSuggestions={handleFetch}
       onSelect={action('selected')}
+      renderOption={renderOption}
     ></AutoComplete>
   )
 }
--- src/components/AutoComplete/autoComplete.tsx
+++ src/components/AutoComplete/autoComplete.tsx
@@ -1,44 +1,69 @@
 import React, { FC, useState, ChangeEvent } from 'react'
 import { Input, InputProps } from '../Input/input'
+import Icon from '../Icon/icon'
 
+interface DataSourceObject {
+  value: string
+}
+export type DataSourceType<T = {}> = T & DataSourceObject
 export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
-  fetchSuggestions: (str: string) => string[]
-  onSelect?: (item: string) => void
+  fetchSuggestions: (
+    str: string
+  ) => DataSourceType[] | Promise<DataSourceType[]>
+  onSelect?: (item: DataSourceType) => void
+  renderOption?: (item: DataSourceType) => JSX.Element
 }
 
 export const AutoComplete: FC<AutoCompleteProps> = (props) => {
-  const { fetchSuggestions, onSelect, value, ...restProps } = props
+  const {
+    fetchSuggestions,
+    onSelect,
+    value,
+    renderOption,
+    ...restProps
+  } = props
 
   const [inputValue, setInputValue] = useState(value)
-  const [suggestions, setSuggestions] = useState<String[]>([])
+  const [suggestions, setSuggestions] = useState<DataSourceType[]>([])
+  const [loading, setLoading] = useState(false)
 
-  console.log(suggestions)
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value.trim()
     setInputValue(value)
     if (value) {
       const results = fetchSuggestions(value)
-      setSuggestions(results)
+      if (results instanceof Promise) {
+        setLoading(true)
+        results.then((data) => {
+          setSuggestions(data)
+          setLoading(false)
+        })
+      } else {
+        setSuggestions(results)
+      }
     } else {
       setSuggestions([])
     }
   }
 
-  const handleSelect = (item: string) => {
-    setInputValue(item)
+  const handleSelect = (item: DataSourceType) => {
+    setInputValue(item.value)
     setSuggestions([])
     if (onSelect) {
       onSelect(item)
     }
   }
 
+  const renderTemplate = (item: DataSourceType) => {
+    return renderOption ? renderOption(item) : item.value
+  }
   const generateDropdown = () => {
     return (
       <ul>
         {suggestions.map((item, index) => {
           return (
             <li key={index} onClick={() => handleSelect(item)}>
-              {item}
+              {renderTemplate(item)}
             </li>
           )
         })}
@@ -48,7 +73,12 @@ export const AutoComplete: FC<AutoCompleteProps> = (props) => {
   return (
     <div className="auto-complete">
       <Input value={inputValue} {...restProps} onChange={handleChange} />
-      {suggestions.length > 0 && generateDropdown()}
+      {loading && (
+        <ul>
+          <Icon icon="spinner" spin />
+        </ul>
+      )}
+      {suggestions && suggestions.length > 0 && generateDropdown()}
     </div>
   )
 }
