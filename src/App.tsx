7d18df485d9f902a62fb470e55b71168a62fa40d
KaiyiWing <Zhang.Kaiyi42@gmail.com>
2020-11-20 14:08:11 +0800

    axios 基础文件上传的示例

--- src/App.tsx
+++ src/App.tsx
@@ -2,38 +2,33 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const App: React.FC = () => {
-  const [title, setTitle] = useState('')
-  const postData = {
-    title: 'my title',
-    body: 'hello man',
+  // 上传文件的基本形式
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files
+    if (files) {
+      const uploadedFile = files[0]
+      // 模拟表单
+      const formData = new FormData()
+      formData.append(uploadedFile.name, uploadedFile)
+      axios
+        .post(
+          'https://run.mocky.io/v3/b5fbe65c-ed29-4cec-a056-8115f0e103d4',
+          formData,
+          {
+            headers: {
+              // 需要在header里指定内容的类型
+              'Content-type': 'multipart/form-data',
+            },
+          }
+        )
+        .then((resp) => {
+          console.log(resp)
+        })
+    }
   }
-  // useEffect(() => {
-  //   axios
-  //     .get('https://jsonplaceholder.typicode.com/posts/1', {
-  //       headers: {
-  //         'X-Requested-With': 'XMLHttpRequest',
-  //       },
-  //       responseType: 'json',
-  //     })
-  //     .then((resp) => {
-  //       console.log(resp)
-  //       setTitle(resp.data.title)
-  //     })
-  // })
-  useEffect(() => {
-    axios
-      .post('https://jsonplaceholder.typicode.com/posts', postData)
-      .then((resp) => {
-        console.log(resp)
-        setTitle(resp.data.title)
-      })
-  })
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>{title}</h1>
-      </header>
+    <div className="App" style={{ marginTop: '100px', marginLeft: '100px' }}>
+      <input type="file" name="myFile" onChange={handleFileChange} />
     </div>
   )
 }
