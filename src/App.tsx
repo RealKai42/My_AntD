e3b9f3431445527e5c3743c7edeccc5234acdd50
KaiyiWing <Zhang.Kaiyi42@gmail.com>
2020-11-20 11:27:39 +0800

    axios get和post的基本使用

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,38 @@
-import React from 'react'
-import Menu from './components/Menu/menu'
-import MenuItem from './components/Menu/menuItem'
-import SubMenu from './components/Menu/subMenu'
-import Transition from './components/Transition/transition'
-import Button from './components/Button/button'
-
-import Icon from './components/Icon/icon'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { useState } from 'react'
-library.add(fas)
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App: React.FC = () => {
-  const [show, setShow] = useState(false)
+  const [title, setTitle] = useState('')
+  const postData = {
+    title: 'my title',
+    body: 'hello man',
+  }
+  // useEffect(() => {
+  //   axios
+  //     .get('https://jsonplaceholder.typicode.com/posts/1', {
+  //       headers: {
+  //         'X-Requested-With': 'XMLHttpRequest',
+  //       },
+  //       responseType: 'json',
+  //     })
+  //     .then((resp) => {
+  //       console.log(resp)
+  //       setTitle(resp.data.title)
+  //     })
+  // })
+  useEffect(() => {
+    axios
+      .post('https://jsonplaceholder.typicode.com/posts', postData)
+      .then((resp) => {
+        console.log(resp)
+        setTitle(resp.data.title)
+      })
+  })
 
   return (
     <div className="App">
       <header className="App-header">
-        <Icon icon="coffee" theme="danger" size="10x" />
-        <Menu
-          defaultIndex={'0'}
-          // mode={'vertical'}
-          defaultOpenSubMenus={['2']}
-          onSelect={(index) => {
-            alert(index)
-          }}
-        >
-          <MenuItem>cool link</MenuItem>
-          <MenuItem disabled>cool link2</MenuItem>
-          <SubMenu title="dropdown">
-            <MenuItem>dropdown 1</MenuItem>
-            <MenuItem>dropdown 2</MenuItem>
-            <MenuItem>dropdown 3</MenuItem>
-          </SubMenu>
-          <MenuItem>cool link3</MenuItem>
-        </Menu>
-        <Button
-          size="lg"
-          onClick={() => {
-            setShow(!show)
-          }}
-        >
-          Toggle
-        </Button>
-        <Transition in={show} timeout={300} animation="zoom-in-left">
-          <div>
-            <p>test test test test test test test test test test test test </p>
-            <p>test test test test test test test test test test test test </p>
-            <p>test test test test test test test test test test test test </p>
-          </div>
-        </Transition>
-
-        <Transition in={show} timeout={300} animation="zoom-in-left" wrapper>
-          <Button btnType="primary" size="lg">
-            A Large Button
-          </Button>
-        </Transition>
+        <h1>{title}</h1>
       </header>
     </div>
   )
