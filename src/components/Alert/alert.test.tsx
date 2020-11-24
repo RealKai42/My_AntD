d6dd285c571c4cb2e2ba9ce76cdb756f9ea8ae1b
KaiyiWing <Zhang.Kaiyi42@gmail.com>
2020-11-24 16:40:09 +0800

    修改了Story的顺序

--- src/components/Alert/alert.test.tsx
+++ src/components/Alert/alert.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
-import Alert, { AlertType, AlertProps } from './alert'
+import Alert, { AlertProps } from './alert'
 
 jest.mock('../Icon', () => {
   return ({ icon, onClick }: { icon: string; onClick: () => {} }) => {
