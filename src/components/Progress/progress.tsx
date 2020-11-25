f80c975fd28430487ccd6aa005b27e6405207d57
KaiyiWing <Zhang.Kaiyi42@gmail.com>
2020-11-25 16:30:55 +0800

    更新了Progress 组件的doc

--- src/components/Progress/progress.tsx
+++ src/components/Progress/progress.tsx
@@ -2,13 +2,21 @@ import React, { FC } from 'react'
 import { ThemeProps } from '../Icon/icon'
 
 export interface ProgressProps {
+  /**设置 Progress 组件显示的百分比 */
   percent: number
+  /**设置 Progress 组件的高度 */
   strokeHight?: number
+  /**设置 Progress 组件是否显示文本 */
   showText?: boolean
+  /**设置 Progress 组件用户自定义的宽度 */
   styles?: React.CSSProperties
+  /**设置 Progress 组件的主题 */
   theme?: ThemeProps
 }
-
+/**
+ * 网页中常用的进度条组件，支持自定义 style、主题等
+ *
+ */
 export const Progress: FC<ProgressProps> = (props) => {
   const { percent, strokeHight, showText, styles, theme } = props
   return (
@@ -29,6 +37,7 @@ export const Progress: FC<ProgressProps> = (props) => {
 }
 
 Progress.defaultProps = {
+  percent: 40,
   strokeHight: 15,
   showText: true,
   theme: 'primary',
